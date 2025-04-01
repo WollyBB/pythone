fcbe62c21ef42f1e5d9d23426fb9a8a6c814f305
WollyBB <wolly94jm@gmail.com>
2025-04-01 13:11:08 +0200

    Create imposta.py

---
+++ imposta.py
@@ -0,0 +1,17 @@
+aliquota1 = 0.23
+aliquota2 = 0.35
+aliquota3 = 0.43
+scaglione1 = 28000
+scaglione2 = 50000
+imposta = 0
+reddito =int(input("inserisci il reddito: "))
+
+if reddito<= scaglione1:
+  imposta = reddito * aliquota1
+else:
+  if reddito > scaglione 1 and reddito <= scaglione2:
+  imposta = reddito ( scaglione1 * aliquota1 + (reddito -scaglioni1) * aliquota2)
+else:
+imposta = ( scaglione1 * aliquota1 + (scaglione2-scaglione1) * aliquota2 + (reddito - scaglione2) * aliquota3)
+print("limposta da pagare è:", imposta)
+
